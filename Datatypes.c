40963a53d88fa56dc260d5ac6fccfd0e51f9ebd5
Ujjawal28 <66429541+Ujjawal28@users.noreply.github.com>
2020-06-04 20:35:17 +0530

    Add files via upload

---
+++ Datatypes.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+void main ()
+{
+
+//Integer
+  unsigned int a = 42949675;
+  printf ("%d", a);
+// new line
+  printf ("\n");
+//Character
+  char ch = 'a';
+  printf ("%c", ch);
+  printf ("\n");
+//Float
+  float b = 10.0;
+  printf ("%f", b);
+   printf ("\n");
+//double
+  double d= 10000000000000000000.0;
+  printf("%lf",d);
+
+
+}
