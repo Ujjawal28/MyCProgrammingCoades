8a7e499978c5a255fe23412f0fac89c57ac98e64
Ujjawal28 <66429541+Ujjawal28@users.noreply.github.com>
2020-06-05 14:07:35 +0530

    Add files via upload

---
+++ EvenNumber.c
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    printf("Even Number between 1 to 100 are :");
+    for(int i=1;i<=100;i++){
+        if(i%2==0){
+            printf("%d\n",i);
+        }
+    }
+    return 0;
+}
