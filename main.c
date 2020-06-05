feba4b8a5e0b5416dd87acd440943c991d2a031f
Ujjawal28 <66429541+Ujjawal28@users.noreply.github.com>
2020-06-05 18:18:42 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+   
+  int x, y, z;
+
+  printf("Enter two numbers to sub\n");
+  scanf("%d%d", &x, &y);
+
+  z = x - y;
+
+  printf("sub of the numbers = %d\n", z);
+
+  return 0;
+}
+
